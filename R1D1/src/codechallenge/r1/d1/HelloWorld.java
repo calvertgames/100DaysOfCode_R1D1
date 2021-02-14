05f479d7cfcc9acdee44e2e5b4f3c1aea5bb49c9
calvertgames <jonathancalvert@gmail.com>
2021-02-13 20:35:49 -0500

    R1D2

---
+++ R1D1/src/codechallenge/r1/d1/HelloWorld.java
@@ -0,0 +1,9 @@
+package codechallenge.r1.d1;
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!");
+	}
+
+}
